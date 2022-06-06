571490f28729d07989ec5fbf7d304b5e46843341
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-06 20:06:53 -0300

    installed styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: red;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }

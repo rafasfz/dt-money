e2bca9bb247513f58341cb1c1399301b860a7ff6
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-11 14:11:46 -0300

    feat: Add create transaction in transaction context

--- src/TransactionContext.tsx
+++ src/TransactionContext.tsx
@@ -10,11 +10,18 @@ interface Transaction {
   createdAt: string;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
-
 interface TrnsactionsProviderProps {
   children: ReactNode;
 }
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export const TransactionsProvider = ({children}: TrnsactionsProviderProps) => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -24,8 +31,12 @@ export const TransactionsProvider = ({children}: TrnsactionsProviderProps) => {
       .then(response => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post('/transactions', transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   );
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionContext';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -12,21 +12,17 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransctionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const {createTransaction} = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
-  const [type, setType] = useState('deposit');
+  const [type, setType] = useState<'deposit' | 'withdraw'>('deposit');
 
   function handleCreateNewTransation(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
-      title,
-      value,
-      category,
-      type
-    };
-    api.post('/transactions', data);
+    createTransaction({title, amount, category, type});
   }
 
   return (
@@ -52,8 +48,8 @@ export function NewTransctionModal({ isOpen, onRequestClose }: NewTransactionMod
         />
 
         <input type="number" placeholder="Valor" 
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
